4ad9ef751bd878b08e369104a327a6f11adc45b6
Shaik Mahaboob Basha <basha.998566@gmail.com>
2024-08-20 22:05:19 +0530

    Frist Commit

---
+++ src/main/java/com/jsp/curdoperation222/Vehicle.java
@@ -0,0 +1,52 @@
+package com.jsp.curdoperation222;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Vehicle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String VehicleNumber;
+	private String VehicleType;
+	@OneToMany
+	@JoinColumn(name="user_id")
+	private User user;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getVehicleNumber() {
+		return VehicleNumber;
+	}
+	public void setVehicleNumber(String vehicleNumber) {
+		VehicleNumber = vehicleNumber;
+	}
+	public String getVehicleType() {
+		return VehicleType;
+	}
+	public void setVehicleType(String vehicleType) {
+		VehicleType = vehicleType;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Object getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
